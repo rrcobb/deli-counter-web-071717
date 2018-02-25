0b0a95c2f36a390b52df629e2b5ad2bacd1b5982
Rob Cobb <rwcobbjr@gmail.com>
2018-02-24 23:53:51 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def line(people=[])
+  if people.empty?
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{people.each_with_index.map { |p, i| "#{i+1}. #{p}" }.join(' ')}"
+  end
+end
+
+def take_a_number(people, person)
+  people.push(person)
+  puts "Welcome, #{person}. You are number #{people.length} in line."
+  people
+end
+
+def now_serving(people)
+  if people.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{people.shift}."
+  end
+  people
+end
